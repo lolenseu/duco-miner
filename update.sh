5b8e4240df302ae8437213cd274a37900d36f659
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-02-23 04:14:12 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cd
 rm -rf DucoMiner
 sudo apt update && sudo apt upgrade -y
-apt install python3 python3-pip git
+sudo apt install python3 python3-pip git
 git clone https://github.com/lolenseu/DucoMiner.git
 cd DucoMiner
 
