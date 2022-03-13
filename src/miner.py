c8504a9156c36bdae185c07637b9ab15d29a8757
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-03-13 23:15:36 +0800

    Update miner.py

--- src/miner.py
+++ src/miner.py
@@ -1,14 +1,10 @@
 import os
 import sys
 import time
-import json
 import socket
 import hashlib
-import urllib.request
 import multiprocessing
-from cgitb import enable
 from datetime import datetime
-from wsgiref.simple_server import server_version
 
 soc = socket.socket()
 soc.settimeout(10)
@@ -17,63 +13,46 @@ with open("src/userinfo") as userinfo:
     username = userinfo.read().rstrip()
 with open("src/thread") as thread:
     thread = thread.read().rstrip()
-processes = []
-    
+
 def getserverip():
-    print("Connecting to the Pool IP Address and Port!")
-    getpool = False
-    while not getpool:
-        try:
-            serverip = ("https://server.duinocoin.com/getPool")
-            poolinfo = json.loads(urllib.request.urlopen(serverip).read())
-            global pooladdress, poolport, poolserver, poolname, poolconnection
-            pooladdress = poolinfo['ip']
-            poolport = poolinfo['port']
-            poolserver = poolinfo['server']
-            poolname = poolinfo['name']
-            poolconnection = poolinfo['success']
-            getpool = True
-        except:
-            print("Connetion Failed!, Retrying...")
-            time.sleep(2.3)
-            continue
-            
-    while True:
-        try:
-            soc.connect((str(51.158.182.90), int(9779)))
-            soc.recv(3).decode()
-            print("")
-            print("Connected!")
-            print("Start Mining...")
-            print("")
+    try:
+        print("Connecting to the Pool IP Address and Port!")
+        pooladdress = "51.158.182.90"
+        poolport = 9779
+        soc.connect((str(pooladdress), int(poolport)))
+        soc.recv(3).decode()
+        print("")
+        print("Connected!")
+        print("Start Mining...")
+        print("")
 
-            while True:
-                soc.send(bytes("JOB," + username + ",LOW", encoding="utf8"))
-                work = soc.recv(1024).decode().rstrip("\n")
-                work = work.split(",")
-                diff = work[2]
-                start = time.time()
-                mainhash = hashlib.sha1(str(work[0]).encode('ascii'))
-                temphash = None
+        while True:
+            soc.send(bytes("JOB," + username + ",LOW", encoding="utf8"))
+            work = soc.recv(1024).decode().rstrip("\n")
+            work = work.split(",")
+            diff = work[2]
+            start = time.time()
+            mainhash = hashlib.sha1(str(work[0]).encode('ascii'))
+            temphash = None
 
-                for result in range(100 * int(diff) + 1):
-                    temphash = mainhash.copy()
-                    temphash.update(str(result).encode('ascii'))
-                    duco = temphash.hexdigest()
+            for result in range(100 * int(diff) + 1):
+                temphash = mainhash.copy()
+                temphash.update(str(result).encode('ascii'))
+                duco = temphash.hexdigest()
 
-                    if work[1] == duco:
-                        stop = time.time()
-                        timediff = stop - start
-                        hashr = result / timediff
-                        soc.send(bytes(str(result) + "," + str(hashr) + ",DucoMiner " + "1.1", encoding="utf8"))
-                        feedback = soc.recv(1024).decode().rstrip("\n")
+                if work[1] == duco:
+                    stop = time.time()
+                    timediff = stop - start
+                    hashr = result / timediff
+                    soc.send(bytes(str(result) + "," + str(hashr) + ",DucoMiner " + "1.1", encoding="utf8"))
+                    feedback = soc.recv(1024).decode().rstrip("\n")
 
-                        if feedback == "GOOD":
-                            print(f"[{datetime.today()}] Share, {result}, Difficulty, {diff}, Hashrate, {int(hashr/1000)}kH/s (Accepted!)")
-                            break
-                        elif feedback == "BAD":
-                            print(f"[{datetime.today()}] Share, {result}, Difficulty, {diff}, Hashrate, {int(hashr/1000)}kH/s (Rejected!)")
-                            break
+                    if feedback == "GOOD":
+                        print(f"[{datetime.today()}] Share, {result}, Difficulty, {diff}, Hashrate, {int(hashr/1000)}kH/s (Accepted!)")
+                        break
+                    elif feedback == "BAD":
+                        print(f"[{datetime.today()}] Share, {result}, Difficulty, {diff}, Hashrate, {int(hashr/1000)}kH/s (Rejected!)")
+                        break
 
     except Exception as e:
         print("")
@@ -83,12 +62,12 @@ def getserverip():
         time.sleep(3)
         os.execl(sys.executable, sys.executable, *sys.argv)
 
-
+processes = []
 for x in range(thread):
     p = multiprocessing.Process(target=getserverip)
     if __name__ == '__main__':
         p.start()
         processes.append(p)
-        
+
 for p in processes:
     p.join()
