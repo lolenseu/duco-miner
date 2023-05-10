755e5322f73f640b770ae0753d143ee0d4cc1e0e
lolenseu <98665691+lolenseu@users.noreply.github.com>
2023-05-10 21:45:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ echo ""
 echo "------------------------"
 echo "|                      |"
 echo "|      DuinoMiner      |"
-echo "|         V1.1         |"
+echo "|         V1.2         |"
 echo "|                      |"
 echo "------------------------"
 echo ""
 echo "Username:"
 read uservar
-echo "Thread:"
-read threadvar
+echo "Key:"
+read keuvar
 echo $uservar > src/userinfo
-echo $threadvar > src/thread
+echo $key > src/key
 echo ""
 python3 src/miner.py
